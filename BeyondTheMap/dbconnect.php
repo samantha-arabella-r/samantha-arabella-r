ee31c8584db1fb194832abe5579d6013692dc0a0
Samantha Arabella Regencia <118825684+samantha-arabella-r@users.noreply.github.com>
2025-04-22 08:59:21 +0800

    Delete BeyondTheMap directory

--- BeyondTheMap/dbconnect.php
+++
@@ -1,8 +0,0 @@
-<?php
-//dbconnect.php
-$con = mysqli_connect("localhost","root","");
-if(!$con){
-	die("Cannot connect". mysqli_error());
-}
-mysqli_select_db($con, "beyondthemap_db") or die("Cannot connect");
-?>
